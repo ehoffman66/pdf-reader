5c42cc01393a149a48188ecbdd504a92b1e5502a
Erik Hoffman <erik.m.hoffman@gmail.com>
2021-11-12 22:08:35 -0500

    output number of pages to the screen

--- pdfread.py
+++ pdfread.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python
+
 import pdfplumber
-with pdfplumber.open(r'formsample.pdf') as pdf:
+
+with pdfplumber.open(r'sample.pdf') as pdf:
     first_page = pdf.pages[0]
     print(first_page.extract_text())
-    print(len(pdf.pages))
+    print("Number of Pages " + str(len(pdf.pages)))
